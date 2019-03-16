8cb18728e28ddaf0f08d9b737434a4dfc49d1f00
Paul Borawski <paulborawski14@gmail.com>
2019-03-16 14:08:51 -0700

    added nodemon server script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello world'));
 
 const port = process.env.PORT || 5000;
 
