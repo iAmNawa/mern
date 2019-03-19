a25432fd57afcda810f28e88697ff725c40685d7
Paul Borawski <paulborawski14@gmail.com>
2019-03-19 07:42:50 -0700

    login and register routes working

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +13,11 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-            <Route exact path="/" component={ Landing } /> 
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
           <Footer />
         </div>
       </Router>
