3da79a7cec6a75da80224201608b637ce038a0b7
Paul Borawski <paulborawski14@gmail.com>
2019-03-21 08:11:59 -0700

    add jwt-decode

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
 import { GET_ERRORS } from './types';
 
 // Register User
@@ -12,3 +13,21 @@ export const registerUser = (userData, history) => dispatch => {
       })
     });
 }
+
+// Login - Get User Token
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    });
+};
