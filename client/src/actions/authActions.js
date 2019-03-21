c36125248d048b6eeac41d3bb0a29aaf33d06fb6
Paul Borawski <paulborawski14@gmail.com>
2019-03-21 08:20:02 -0700

    implemented login in redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
-import { GET_ERRORS } from './types';
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -24,6 +25,10 @@ export const loginUser = (userData) => dispatch => {
       localStorage.setItem('jwtToken', token);
       // Set token to Auth header
       setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err => {
       dispatch({
@@ -31,3 +36,11 @@ export const loginUser = (userData) => dispatch => {
         payload: err.response.data
     });
 };
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
