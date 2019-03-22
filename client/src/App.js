c88e60a63c01e66863fbba33085847684eb5f0ab
Paul Borawski <paulborawski14@gmail.com>
2019-03-22 10:16:05 -0700

    logoutUser working

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import{ setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 import Navbar from './components/layout/Navbar';
@@ -14,9 +14,9 @@ import './App.css';
 
 // Check for token
 if (localStorage.jwtToken) {
-  // set auth token header auth
+  // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
-  // Decode token and get user info and expiration
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
@@ -25,8 +25,8 @@ if (localStorage.jwtToken) {
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     // Logout user
-    let logoutUser = store.dispatch.logoutUser;
     store.dispatch(logoutUser());
+    // Redirect to login
     window.location.href = '/login';
   }
 }
