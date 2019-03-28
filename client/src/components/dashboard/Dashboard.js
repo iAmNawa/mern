4d182cafd55c77aa1e90b08063b397a0a0724ebb
Paul Borawski <paulborawski14@gmail.com>
2019-03-28 09:19:18 -0700

    setup profile if you haven't yet

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import  { getCurrentProfile } from '../../actions/profileActions';
@@ -16,14 +17,28 @@ class Dashboard extends Component {
     if (profile === null || loading) {
       dashboardContent = <Spinner />
     } else {
-      dashboardContent = <h1>Hello</h1>
+      // Check if loggedf in user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>DISPLAY PROFILE</h4>
+      } else {
+        // User is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome { user.name }</p>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
+      }
     }
     return (
       <div className ="dashboard">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 classname="display-4">Dashboard</h1>
+              <h1 className="display-4">Dashboard</h1>
               {dashboardContent}
             </div>
           </div>
