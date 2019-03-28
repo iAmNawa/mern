a4fc49ce1cbc95e5c16ce310c997f90b55e3295b
Paul Borawski <paulborawski14@gmail.com>
2019-03-28 08:20:48 -0700

    Spinner when page is loading

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import  { getCurrentProfile } from '../../actions/profileActions';
+import Spinner from '../common/Spinner';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -9,12 +10,38 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+    let dashboardContent;
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />
+    } else {
+      dashboardContent = <h1>Hello</h1>
+    }
     return (
-      <div>
-        Dashboard
+      <div className ="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 classname="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default connect(null, { getCurrentProfile })(Dashboard);
+Dashboard.propType = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
