19572c6425a5ae347af5cdd228c47be10fbdfa48
Paul Borawski <paulborawski14@gmail.com>
2019-03-28 17:13:11 -0700

    create-profile route created -- private route

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -27,7 +27,19 @@ class CreateProfile extends Component {
 
   render() {
     return (
-      <div></div>
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Let's get some information to make your profile stand out
+              </p>
+              <small className="d-block pb-3">* = required fields</small>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
